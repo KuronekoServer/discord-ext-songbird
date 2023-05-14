b24ca70e553e865b9524a30c8383c5d37951d4ef
mc_fdc <Masato.04.11.2007@gmail.com>
2023-05-14 17:50:53 +0900

    Update client.py

--- dext_songbird/client.py
+++ dext_songbird/client.py
@@ -1,2 +1,9 @@
 from discord import VoiceProtocol
-from .dext import Connector
+from .dext import VoiceConnector
+
+
+class VoiceClient(VoiceProtocol):
+    
+    def __init__(self, client, channel):
+        super().__init__(client, channel)
+        self._connector = VoiceConnector()
